cc38a239af012bc320593126937c41e35cd7e22c
andrii <andro999b@gmail.com>
2018-12-09 18:30:01 +0200

    fix animevost

--- server/service/extractVideo/animevostExtractor.js
+++ server/service/extractVideo/animevostExtractor.js
@@ -1,6 +1,6 @@
 const regExprExtractor = require('./regExprExtractor')
 
 module.exports = regExprExtractor([
-    /\[HD\](http:\/\/hd\.aniland\.org\/[0-9]+\/[0-9]+\.mp4\?md5=[a-zA-Z0-9-_]+&time=[0-9]+)+/,
-    /\[SD\](http:\/\/std\.aniland\.org\/[0-9]+\.mp4\?md5=[a-zA-Z0-9-_]+&time=[0-9]+)+/,
+    /\[HD[^\]]*\](http:\/\/hd\.aniland\.org\/[0-9]+\/[0-9]+\.mp4\?md5=[a-zA-Z0-9-_]+&time=[0-9]+)+/,
+    /\[SD[^\]]*\](http:\/\/std\.aniland\.org\/[0-9]+\.mp4\?md5=[a-zA-Z0-9-_]+&time=[0-9]+)+/,
 ])
